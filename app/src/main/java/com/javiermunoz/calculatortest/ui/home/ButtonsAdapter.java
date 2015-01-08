7df1eea749498cffa66862cf0c0ee9c7b04aff84
Hector de Isidro <hector.isidro@mobusi.com>
2015-01-08 16:24:52 +0100

    (All) Buttons functionality developed

--- app/src/main/java/com/javiermunoz/calculatortest/ui/home/ButtonsAdapter.java
+++ app/src/main/java/com/javiermunoz/calculatortest/ui/home/ButtonsAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.TextView;
 import com.javiermunoz.calculatortest.R;
 
@@ -14,43 +13,36 @@ import com.javiermunoz.calculatortest.R;
  */
 public class ButtonsAdapter extends BaseAdapter {
 
-  private Context context;
-  private LayoutInflater inflater;
+    private LayoutInflater inflater;
 
-  static final String[] numbers = new String[] {
-      "7", "8", "9", "/", "4", "5", "6", "X", "1", "2", "3", "-", ",", "0", "=", "+"
-  };
-
-  public ButtonsAdapter(Context context) {
-    this.context = context;
-    inflater = LayoutInflater.from(context);
-  }
-
-  @Override public int getCount() {
-    return numbers.length;
-  }
-
-  @Override public Object getItem(int position) {
-    return null;
-  }
-
-  @Override public long getItemId(int position) {
-    return 0;
-  }
-
-  @Override public View getView(int position, View convertView, ViewGroup parent) {
-    TextView button;
-    if (convertView == null) {  // if it's not recycled, initialize some attributes
-      button = (TextView) newView(inflater, position, parent);
-    } else {
-      button = (TextView) convertView;
+    public ButtonsAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
     }
-    button.setText(numbers[position]);
-    return button;
-  }
 
-  private View newView(LayoutInflater inflater, int position, ViewGroup parent) {
-    return inflater.inflate(R.layout.view_button_item, parent, false);
-  }
+    @Override
+    public int getCount() {
+        return AppConstants.numbers.length;
+    }
 
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView button;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            button = (TextView) inflater.inflate(R.layout.view_button_item, parent, false);
+        } else {
+            button = (TextView) convertView;
+        }
+        button.setText(AppConstants.numbers[position]);
+        return button;
+    }
 }
--- app/src/main/java/com/javiermunoz/calculatortest/ui/home/CalculatorActivity.java
+++ app/src/main/java/com/javiermunoz/calculatortest/ui/home/CalculatorActivity.java
@@ -1,162 +1,147 @@
 package com.javiermunoz.calculatortest.ui.home;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.javiermunoz.calculatortest.R;
 
 public class CalculatorActivity extends ActionBarActivity {
 
-  private GridView gridView;
-  private TextView mainTextView;
-  private boolean isCommaSet = false;
+    private GridView gridView;
+    private TextView mainTextView;
 
-  private String firstNumber = "";
-  private String secondNumber = "";
-  private String operator = "";
+    private String firstNumber = "";
+    private String operator = "";
 
-  private final int BUTTON_0 = 13;
-  private final int BUTTON_1 = 8;
-  private final int BUTTON_2 = 9;
-  private final int BUTTON_3 = 10;
-  private final int BUTTON_4 = 4;
-  private final int BUTTON_5 = 5;
-  private final int BUTTON_6 = 6;
-  private final int BUTTON_7 = 0;
-  private final int BUTTON_8 = 1;
-  private final int BUTTON_9 = 2;
-  private final int BUTTON_PLUS = 15;
-  private final int BUTTON_MINUS = 11;
-  private final int BUTTON_MULTP = 7;
-  private final int BUTTON_DIV = 3;
-  private final int BUTTON_COMMA = 12;
-  private final int BUTTON_EQUALS = 14;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_calculator);
+        mainTextView = (TextView) findViewById(R.id.resultTextView);
+        mainTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainTextView.setText("0");
+                firstNumber = "";
+                operator = "";
 
-    mainTextView = (TextView) findViewById(R.id.resultTextView);
+            }
+        });
 
-    gridView = (GridView) findViewById(R.id.buttonsGridView);
+        gridView = (GridView) findViewById(R.id.buttonsGridView);
+        gridView.setAdapter(new ButtonsAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                buttonPressed(position);
+            }
+        });
 
-    gridView.setAdapter(new ButtonsAdapter(this));
 
-    gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-      public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-        buttonPressed(position);
-      }
-    });
-  }
+    }
 
-  private void buttonPressed(int position) {
-    switch (position) {
-      case BUTTON_0:
-        writeInMainText("0");
-        break;
-      case BUTTON_1:
-        writeInMainText("1");
-        break;
-      case BUTTON_2:
-        writeInMainText("2");
-        break;
-      case BUTTON_3:
-        writeInMainText("3");
-        break;
-      case BUTTON_4:
-        writeInMainText("4");
-        break;
-      case BUTTON_5:
-        writeInMainText("5");
-        break;
-      case BUTTON_6:
-        writeInMainText("6");
-        break;
-      case BUTTON_7:
-        writeInMainText("7");
-        break;
-      case BUTTON_8:
-        writeInMainText("8");
-        break;
-      case BUTTON_9:
-        writeInMainText("9");
-        break;
-      case BUTTON_PLUS:
-        sumFunctionallity();
-        break;
-      case BUTTON_MINUS:
-        mainTextView.setText("0");
-        break;
-      case BUTTON_MULTP:
-        mainTextView.setText("0");
-        break;
-      case BUTTON_DIV:
-        mainTextView.setText("0");
-        break;
-      case BUTTON_COMMA:
-        if (!isCommaSet) {
-          isCommaSet = true;
-          mainTextView.setText(mainTextView.getText() + ",");
+    private void buttonPressed(int position) {
+        switch (AppConstants.numbers[position]) {
+            case AppConstants.BUTTON_PLUS:
+            case AppConstants.BUTTON_MINUS:
+            case AppConstants.BUTTON_MULTP:
+            case AppConstants.BUTTON_DIV:
+                calculateFunctionallity(AppConstants.numbers[position]);
+                break;
+
+            case AppConstants.BUTTON_DOT:
+                if (!mainTextView.getText().toString().contains(AppConstants.BUTTON_DOT)) {
+                    mainTextView.setText(mainTextView.getText() + AppConstants.BUTTON_DOT);
+                }
+                break;
+
+            case AppConstants.BUTTON_EQUALS:
+                equalsFunctionallity();
+                break;
+
+            default:
+                writeInMainText(AppConstants.numbers[position]);
         }
-        break;
-      case BUTTON_EQUALS:
-        equalsFunctionallity();
-        break;
     }
-  }
 
-  private void equalsFunctionallity() {
-    String actuallyInMain = mainTextView.getText().toString();
-    if (!firstNumber.equalsIgnoreCase("")) {
-      if (operator.equalsIgnoreCase("+")) {
-        int result = (Integer.parseInt(firstNumber) + Integer.parseInt(actuallyInMain));
-        mainTextView.setText("" + result);
-      }
+
+    private void calculateFunctionallity(String operator) {
+        String currentlyInMain = mainTextView.getText().toString();
+        this.operator = operator;
+        if (currentlyInMain.equalsIgnoreCase("0")) {
+            if (!firstNumber.equalsIgnoreCase("")) {
+                mainTextView.setText(firstNumber);
+            } else {
+                mainTextView.setText("0");
+            }
+        } else {
+            if (!firstNumber.equalsIgnoreCase("")) {
+                float result = 0;
+                if (operator.equalsIgnoreCase(AppConstants.BUTTON_PLUS)) {
+                    result = (Float.parseFloat(firstNumber) + Float.parseFloat(currentlyInMain));
+                } else if (operator.equalsIgnoreCase(AppConstants.BUTTON_MINUS)) {
+                    result = (Float.parseFloat(firstNumber) - Float.parseFloat(currentlyInMain));
+                } else if (operator.equalsIgnoreCase(AppConstants.BUTTON_MULTP)) {
+                    result = (Float.parseFloat(firstNumber) * Float.parseFloat(currentlyInMain));
+                } else if (operator.equalsIgnoreCase(AppConstants.BUTTON_DIV)) {
+                    result = (Float.parseFloat(firstNumber) / Float.parseFloat(currentlyInMain));
+                } else {
+                    return;
+                }
+
+                mainTextView.setText(String.valueOf(result));
+            } else {
+                firstNumber = mainTextView.getText().toString();
+                mainTextView.setText("0");
+            }
+        }
     }
-  }
 
-  private void sumFunctionallity() {
-    String actuallyInMain = mainTextView.getText().toString();
-    operator = "+";
-    if (actuallyInMain.equalsIgnoreCase("0")) {
-      if (!firstNumber.equalsIgnoreCase("")) {
-        mainTextView.setText(firstNumber);
-      } else {
-        mainTextView.setText("0");
-      }
-    } else {
-      if (!firstNumber.equalsIgnoreCase("")) {
-        int result = Integer.parseInt(firstNumber) + Integer.parseInt(actuallyInMain);
-        mainTextView.setText(result);
-      } else {
-        firstNumber = mainTextView.getText().toString();
-        mainTextView.setText("0");
-      }
+
+    private void equalsFunctionallity() {
+        String currentlyInMain = mainTextView.getText().toString();
+
+        if (!firstNumber.equalsIgnoreCase("") && !currentlyInMain.equalsIgnoreCase("0")) {
+            try {
+                float result = 0;
+                if (operator.equalsIgnoreCase(AppConstants.BUTTON_PLUS)) {
+                    result = (Float.parseFloat(firstNumber) + Float.parseFloat(currentlyInMain));
+                } else if (operator.equalsIgnoreCase(AppConstants.BUTTON_MINUS)) {
+                    result = (Float.parseFloat(firstNumber) - Float.parseFloat(currentlyInMain));
+                } else if (operator.equalsIgnoreCase(AppConstants.BUTTON_MULTP)) {
+                    result = (Float.parseFloat(firstNumber) * Float.parseFloat(currentlyInMain));
+                } else if (operator.equalsIgnoreCase(AppConstants.BUTTON_DIV)) {
+                    result = (Float.parseFloat(firstNumber) / Float.parseFloat(currentlyInMain));
+                } else {
+                    return;
+                }
+
+                if (result == Math.round(result)) {
+                    // integer
+                    mainTextView.setText(String.valueOf(Math.round(result)));
+                } else {
+                    mainTextView.setText(String.valueOf(result));
+                }
+            } catch (Exception ignored) {
+            }
+        }
     }
-  }
 
-  private void writeInMainText(String text) {
-    String comparingWith = mainTextView.getText().toString();
-    if (!comparingWith.equalsIgnoreCase("0")) {
-      mainTextView.setText(mainTextView.getText() + text);
-    } else if (comparingWith.equalsIgnoreCase("+")
-        || comparingWith.equalsIgnoreCase("-")
-        || comparingWith.equalsIgnoreCase("*")
-        || comparingWith.equalsIgnoreCase("/")
-        || comparingWith.equalsIgnoreCase("=")
-        || comparingWith.equalsIgnoreCase(",")) {
+    private void writeInMainText(String text) {
+        String comparingWith = mainTextView.getText().toString();
+        if (!comparingWith.equalsIgnoreCase("0")) {
+            mainTextView.setText(mainTextView.getText() + text);
+        } else if (!comparingWith.equalsIgnoreCase("+")
+            && !comparingWith.equalsIgnoreCase("-")
+            && !comparingWith.equalsIgnoreCase("*")
+            && !comparingWith.equalsIgnoreCase("/")
+            && !comparingWith.equalsIgnoreCase("=")
+            && !comparingWith.equalsIgnoreCase(",")) {
 
-    } else {
-      mainTextView.setText(text);
+            mainTextView.setText(text);
+        }
     }
-  }
 }
